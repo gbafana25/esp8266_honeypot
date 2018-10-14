0cee28a1c38b5afd56d0361b1a5033b5a498cd0b
gbafana25 <34496948+gbafana25@users.noreply.github.com>
2018-10-14 13:54:29 -0500

    added commments to ampy_config

--- ampy_config.bat
+++ ampy_config.bat
@@ -1,3 +1,5 @@
+rem this batch file sets up ampy to connect to the microcontroller in your current directory
+rem specify COM port as needed
 set AMPY_PORT=COM3
 set AMPY_BAUD=115200
 set AMPY_DELAY=0.5
